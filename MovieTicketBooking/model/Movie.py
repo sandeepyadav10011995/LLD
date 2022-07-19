3ed9eab7ce7a7f213d1d98c42f0478142dc9bc0f
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2022-07-19 17:21:51 +0530

    Create Movie.py

---
+++ MovieTicketBooking/model/Movie.py
@@ -0,0 +1,15 @@
+
+
+class Movie:
+    def __init__(self, id: str, name: str) -> None:
+        self.id = id
+        self.name = name
+
+    def getId(self):
+        return self.id
+
+    def getName(self):
+        return self.name
+
+    # Other Metadata
+    
