80f77456264c7399cc327fa2dea5f162856b2834
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2022-07-22 07:04:59 +0530

    Update Mode.py

--- ParkingLot/mode/Mode.py
+++ ParkingLot/mode/Mode.py
@@ -1,1 +1,23 @@
 
+from abc import ABCMeta, abstractmethod
+
+from OutputPrinter import OutputPrinter
+from command.CommandExecutorFactory import CommandExecutorFactory
+from model.Command import Command
+
+
+class Mode(metaclass=ABCMeta):
+    def __init__(self, commandExecutorFactory: CommandExecutorFactory, outputPrinter: OutputPrinter):
+        self.commandExecutorFactory = commandExecutorFactory
+        self.outputPrinter = outputPrinter
+
+    def processCommand(self, command: Command) -> None:
+        commandExecutor = self.commandExecutorFactory.getCommandExecutor(command=command)
+        if commandExecutor.validate(command=command)
+            commandExecutor.execute(command=command)
+        else:
+            pass
+
+    @abstractmethod
+    def process(self):
+        pass
