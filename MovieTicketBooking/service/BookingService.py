87e76264f3f3113c6e93acf9d6daa55ab1a3e503
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2022-07-22 05:50:46 +0530

    Update BookingService.py

--- MovieTicketBooking/service/BookingService.py
+++ MovieTicketBooking/service/BookingService.py
@@ -4,68 +4,51 @@ from model.BokkingStatus import BookingStatus
 from model.Booking import Booking
 from model.Seat import Seat
 from model.Show import Show
+from providers.SeatLockProvider import SeatLockProvider
 
 
 class BookingService:
-    def __init__(self, seatLockProvider):
-        self.seatLockProvider = seatLockProvider
-        self.showBookings = dict()
+    def __init__(self, seatLockProvider: SeatLockProvider) -> None:
+        self._seatLockProvider = seatLockProvider
+        self._showBookings = dict()
 
     def getBookings(self, bookingId: str) -> Booking:
-        if bookingId not in self.showBookings:
+        if bookingId not in self._showBookings:
             pass
-        return self.showBookings.get(bookingId)
+        return self._showBookings.get(bookingId)
 
-    def getAllBookings(self, show) -> list[Booking]:
+    def getAllBookings(self, show: Show) -> list[Booking]:
         allBookingsForShow = []
-        for booking in self.showBookings:
+        for booking in self._showBookings:
             if booking.getShow() == show:
                 allBookingsForShow.append(booking)
 
         return allBookingsForShow
 
     def createBooking(self, userId: str, show: Show, seats: list[Seat]) -> Booking:
-        if self.isAnySeatAlreadyBooked(show, seats):
+        if self._isAnySeatAlreadyBooked(show, seats):
             pass
-        self.seatLockProvider.lockSeats(show, seats, userId)
+        self._seatLockProvider.lockSeats(show, seats, userId)
         bookingId = str(uuid.uuid4())
         newBooking = Booking(id=bookingId, show=show, seatsBooked=seats, user=userId)
-        self.showBookings[bookingId] = newBooking
+        self._showBookings[bookingId] = newBooking
         return newBooking
 
     def getBookedSeats(self, show: Show) -> list[Seat]:
         allBookings = self.getAllBookings(show)
-        return [booking.getSeatsBooked() for booking in allBookings if booking.bookingStatus == BookingStatus.Confirmed]
+        return [booking.getSeatsBooked() for booking in allBookings if booking.getBookingStatus() == BookingStatus.Confirmed]
 
     def confirmBooking(self, booking: Booking, user: str) -> None:
         if not booking.getUser() == user:
             pass
         for seat in booking.getSeatsBooked():
-            if not self.seatLockProvider.validateLock(booking.getShow(), seat, user):
+            if not self._seatLockProvider.validateLock(booking.getShow(), seat, user):
                 pass
             booking.confirmBooking()
 
-    def isAnySeatAlreadyBooked(self, show: Show, seats: list[Seat]) -> bool:
+    def _isAnySeatAlreadyBooked(self, show: Show, seats: list[Seat]) -> bool:
         bookedSeats = self.getBookedSeats(show)
         for seat in seats:
             if seat in bookedSeats:
                 return True
         return False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
