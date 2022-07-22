6a4a4589d62028781a79dcd2f04f73785c6b642e
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2022-07-22 07:44:19 +0530

    Create cart.py

---
+++ Swiggy/models/cart.py
@@ -0,0 +1,15 @@
+class Cart:
+    def __init__(self):
+        self.items = []
+
+    def addItem(self, item):
+        self.items.append(item)
+
+    def removeItem(self, item):
+        self.items.remove(item)
+
+    def getItems(self):
+        return self.items
+
+    def clearCart(self):
+        self.items = []
