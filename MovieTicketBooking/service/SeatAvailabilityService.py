8c017ed53a97ba62367bea003698790a276d10cd
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2022-07-22 05:51:53 +0530

    Update SeatAvailabilityService.py

--- MovieTicketBooking/service/SeatAvailabilityService.py
+++ MovieTicketBooking/service/SeatAvailabilityService.py
@@ -1,16 +1,17 @@
 from model.Seat import Seat
 from model.Show import Show
+from providers.SeatLockProvider import SeatLockProvider
 from service.BookingService import BookingService
 
 
 class SeatAvailabilityService:
-    def __init__(self, bookingService: BookingService, seatLockProvider) -> None:
-        self.bookingService = bookingService
-        self.seatLockProvider = seatLockProvider
+    def __init__(self, bookingService: BookingService, seatLockProvider: SeatLockProvider) -> None:
+        self._bookingService = bookingService
+        self._seatLockProvider = seatLockProvider
 
     def getUnavailableSeats(self, show: Show) -> list[Seat]:
-        unavailableSeats = self.bookingService.getBookedSeats()
-        unavailableSeats.append(self.seatLockProvider.getLockedSeats(show))
+        unavailableSeats = self._bookingService.getBookedSeats(show=show)
+        unavailableSeats.extend(self._seatLockProvider.getLockedSeats(show=show))
         return unavailableSeats
 
     def getAvailableSeats(self, show: Show) -> list[Seat]:
