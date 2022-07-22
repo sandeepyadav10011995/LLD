df705509eb21ded80d17d85d7767dcdef268dfc4
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2022-07-22 05:51:08 +0530

    Update MovieService.py

--- MovieTicketBooking/service/MovieService.py
+++ MovieTicketBooking/service/MovieService.py
@@ -5,15 +5,15 @@ from model.Movie import Movie
 
 class MovieService:
     def __init__(self) -> None:
-        self.movies = dict()
+        self._movies = dict()
 
     def getMovie(self, movieId: str) -> Movie:
-        if movieId not in self.movies:
+        if movieId not in self._movies:
             pass
-        return self.movies.get(movieId)
+        return self._movies.get(movieId)
 
     def createMovie(self, movieName: str) -> Movie:
         movieId = str(uuid.uuid4())
         movie = Movie(id=movieId, name=movieName)
-        self.movies[movieId] = movie
+        self._movies[movieId] = movie
         return movie
