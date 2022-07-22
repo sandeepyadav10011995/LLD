cd29a504dffa2d2c23e1d7ea0316ec4046ac2db3
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2022-07-22 08:33:17 +0530

    Create exceptions.py

---
+++ Cab Booking/exceptions.py
@@ -0,0 +1,26 @@
+
+
+class CabAlreadyExitsException(Exception):
+    pass
+
+
+class CabNotFoundException(Exception):
+    pass
+
+
+class NoCabsAvailableException(Exception):
+    pass
+
+
+class RiderAlreadyExistsException(Exception):
+    pass
+
+
+class RiderNotFoundException(Exception):
+    pass
+
+
+class TripNotFoundException(Exception):
+    pass
+
+
