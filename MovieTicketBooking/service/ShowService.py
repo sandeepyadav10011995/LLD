524cbeb6382727315e3b1b238cc34d1ef0159ffc
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2022-07-22 05:52:16 +0530

    Update ShowService.py

--- MovieTicketBooking/service/ShowService.py
+++ MovieTicketBooking/service/ShowService.py
@@ -8,30 +8,30 @@ from model.Show import Show
 
 class ShowService:
     def __init__(self):
-        self.shows = dict()
+        self._shows = dict()
 
     def getShow(self, showId: str) -> Show:
-        if showId not in self.shows:
+        if showId not in self._shows:
             pass
-        return self.shows.get(showId)
+        return self._shows.get(showId)
 
     def createShow(self, movie: Movie, screen: Screen, startTime: datetime, durationInSeconds: int) -> Show:
-        if not self.checkIfShowCreationAllowed(screen, startTime, durationInSeconds):
+        if not self._checkIfShowCreationAllowed(screen, startTime, durationInSeconds):
             pass
         showId = str(uuid.uuid4())
         show = Show(id=showId, movie=movie, screen=screen, startTime=startTime, durationInSeconds=durationInSeconds)
-        self.shows[showId] = show
+        self._shows[showId] = show
         return show
 
     def getShowsForScreen(self, screen: Screen) -> list[Show]:
         allShowsForScreen = []
-        for show in self.shows:
+        for show in self._shows:
             if show.getScreen() == screen:
                 allShowsForScreen.append(show)
 
         return allShowsForScreen
 
     @staticmethod
-    def checkIfShowCreationAllowed(screen, startTime, durationInSeconds) -> bool:
+    def _checkIfShowCreationAllowed(screen, startTime, durationInSeconds) -> bool:
         # Write the Logic
         return True
