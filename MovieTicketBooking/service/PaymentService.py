4088091c8eadc26a0337c2320248d0c105058fe8
Sandeep Yadav <sandeepyadav10011995@gmail.com>
2022-07-22 05:51:29 +0530

    Update PaymentService.py

--- MovieTicketBooking/service/PaymentService.py
+++ MovieTicketBooking/service/PaymentService.py
@@ -1,20 +1,21 @@
 from model.Booking import Booking
+from providers.SeatLockProvider import SeatLockProvider
 
 
 class PaymentService:
-    def __init__(self, allowedRetries: int, seatLockProvider) -> None:
-        self.allowedRetries = allowedRetries
-        self.seatLockProvider = seatLockProvider
-        self.bookingFailures = dict()
+    def __init__(self, allowedRetries: int, seatLockProvider: SeatLockProvider) -> None:
+        self._allowedRetries = allowedRetries
+        self._seatLockProvider = seatLockProvider
+        self._bookingFailures = dict()
 
     def processPaymentFailed(self, booking: Booking, user: str) -> None:
         if not booking.getUser() == user:
             pass
-        if booking not in self.bookingFailures:
-            self.bookingFailures[0] = booking
+        if booking not in self._bookingFailures:
+            self._bookingFailures[booking] = 0
 
-        currentFailuresCount = self.bookingFailures.get(booking)
+        currentFailuresCount = self._bookingFailures.get(booking)
         newFailuresCount = currentFailuresCount + 1
-        self.bookingFailures[booking] = newFailuresCount
-        if newFailuresCount > self.allowedRetries:
-            self.seatLockProvider.unlockSeats(booking.getShow(), booking.getSeatsBooked(), booking.getUser())
+        self._bookingFailures[booking] = newFailuresCount
+        if newFailuresCount > self._allowedRetries:
+            self._seatLockProvider.unlockSeats(show=booking.getShow(), seats=booking.getSeatsBooked(), user=booking.getUser())
